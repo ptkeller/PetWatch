637cb8e6c0ad4d55e4e2e7a1ff025e871ac8414f
ptkeller <p_keller@sbcglobal.net>
2015-09-30 22:09:45 -0700

    [Progress] successfully accessed database and saved changes

--- client/owner-list.jsx
+++ client/owner-list.jsx
@@ -6,11 +6,35 @@ var OwnerProfile = require('./owner-profile');
 
 
 module.exports = React.createClass({
-  getInitialState: function(){
+  // getInitialState: function(){
+  //     return {
+  //       owners: [{firstname: 'Nate', lastname: 'Kim', pet: '',description: 'Cat killer'},{firstname: 'Nate', lastname: 'Kim', pet: '',description: 'Cat killer'},{firstname: 'Nate', lastname: 'Kim', pet: '',description: 'Cat killer'}],
+  //       pets: [{name: 'FEEFEEDO', species: 'catdog', size: '2'},{name: 'FEEFEEDO', species: 'catdog', size: '2'},{name: 'FEEFEEDO', species: 'catdog', size: '2'}]
+  //     }
+  //   },
+  getInitialState: function() {
       return {
-        owners: [{firstname: 'Nate', lastname: 'Kim', pet: '',description: 'Cat killer'},{firstname: 'Nate', lastname: 'Kim', pet: '',description: 'Cat killer'},{firstname: 'Nate', lastname: 'Kim', pet: '',description: 'Cat killer'}],
-        pets: [{name: 'FEEFEEDO', species: 'catdog', size: '2'},{name: 'FEEFEEDO', species: 'catdog', size: '2'},{name: 'FEEFEEDO', species: 'catdog', size: '2'}]
+        items: []
       }
+    },
+
+    componentWillMount: function() {
+      console.log('this is doing stuff')
+      this.firebaseRef = new Firebase('https://petwatch.firebaseio.com/owners');
+      this.firebaseRef.on('value', function(dataSnapshot) {
+        console.log(dataSnapshot, 'this should be snapshot');
+        var items = [];
+        dataSnapshot.forEach(function(childSnapshot) {
+          var item = childSnapshot.val();
+          item['.key'] = childSnapshot.key();
+          items.push(item);
+          console.log(items, 'this shoudl be items');
+        }.bind(this));
+
+        this.setState({
+          items: items
+        });
+      }.bind(this));
     },
   render: function(){
     return <div className='ownerBar'>
@@ -18,8 +42,9 @@ module.exports = React.createClass({
     </div>
   },
   renderOwners: function() {
-    return this.state.owners.map(function(owner){
-       return <OwnerProfile firstname={owner.firstname}
+    return this.state.items.map(function(owner){
+      console.log(owner.pets, 'this is owner')
+       return <OwnerProfile firstname={owner.firstname} pic={owner.pets.picURL} 
         lastname={owner.lastname} />
     })
   }
--- client/owner-profile.jsx
+++ client/owner-profile.jsx
@@ -3,39 +3,7 @@
 //more pictures, possible rating?
 var React = require('react');
 
-
 module.exports = React.createClass({
-
-	getInitialState: function(){
-return {
-		ownerItem: []
-		}
-	},
-	componentWillMount: function(){
-		this.firebaseRef = new Firebase('https://petwatch.firebaseio.com/owners/');
-     this.firebaseRef.on('value', function(dataSnapshot) {
-			 console.log(dataSnapshot)
-       var items = [];
-       dataSnapshot.forEach(function(childSnapshot) {
-				 console.log(childSnapshot, 'this is child');
-         var item = childSnapshot.val();
-				 console.log(item, 'this is itme')
-         item['.key'] = childSnapshot.key();
-         items.push(item);
-				 console.log(items, 'this is items at the end')
-       }.bind(this));
-
-       this.setState({
-         ownerItem: items
-       });
-     }.bind(this));
-		 console.log(this.state.ownerItem, 'this is the array in state')
-   },
-	 
-	 componentWillUnmount: function() {
-     this.firebaseRef.off();
-   },
-
 
 	render: function(){
 		return (
@@ -48,9 +16,10 @@ return {
   renderOwnerProfile: function(){
       return (
         <div className='ownerThumbnail'>
-          <img src={this.state.data.pets.picUrl}/>
-          <div className='ownerCaption'>
-            <h4>{this.state.data.firstname} {this.state.data.lastname} </h4>
+          <img src={this.props.pic} />
+
+					<div className='ownerCaption'>
+            <h4>{this.props.firstname} {this.props.lastname} </h4>
           </div>
         </div>
       )
